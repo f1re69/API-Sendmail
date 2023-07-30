dce00b52ca4d4f2113fc510e5920d39c843b7cf0
Khelil Bezzouh <kbezzouh@gmail.com>
2023-07-30 12:12:04 +0200

    Add CORS_ORIGIN environment variable for dynamic CORS configuration.

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,7 @@ const email_1 = __importDefault(require("./routes/email")); // Import using ES6 
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with your frontend URL
+    origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
